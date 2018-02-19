5afef8060cafc3d5c6ed4e2c31ac5717cdda5447
Nadir Ashraf <nadir.ashraf@xublimity.com>
2018-02-19 10:20:45 +0500

    added demo9 custom layout. Will compile sources and add more demos now

---
+++ dist/xDS9/src/app/theme/layouts/aside-right/aside-right.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Helpers } from '../../../helpers';
+
+
+@Component({
+selector: "app-aside-right",
+templateUrl: "./aside-right.component.html",
+encapsulation: ViewEncapsulation.None,
+})
+export class AsideRightComponent implements OnInit {
+
+
+constructor()  {
+
+}
+ngOnInit()  {
+
+}
+
+}
---
+++ dist/xDS9/src/app/theme/pages/loader-enabled-enabled-type-spinner-message/loader-enabled-enabled-type-spinner-message.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Helpers } from '../../../helpers';
+import { ScriptLoaderService } from '../../../_services/script-loader.service';
+
+
+@Component({
+selector: ".m-grid__item.m-grid__item--fluid.m-grid.m-grid--hor-desktop.m-grid--desktop.m-body",
+templateUrl: "./loader-enabled-enabled-type-spinner-message.component.html",
+encapsulation: ViewEncapsulation.None,
+})
+export class LoaderEnabledEnabledTypeSpinnerMessageComponent implements OnInit {
+
+
+constructor()  {
+
+}
+ngOnInit()  {
+
+}
+
+}
