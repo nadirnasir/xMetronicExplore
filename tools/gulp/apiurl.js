568b96c45416f03871dc17b3c8418178d904b73a
Nadir Ashraf <nadir.ashraf@xublimity.com>
2018-02-27 15:56:48 +0500

    Modified apiurl.js to replace host url instead of prepend

--- tools/gulp/apiurl.js
+++ tools/gulp/apiurl.js
@@ -3,18 +3,27 @@ var build = require('./build');
 var replace = require('gulp-replace');
 var func = require('./helpers');
 
-const PREG_APIURL = new RegExp(/["|'](inc\/api.*?)["|']/);
+// Original path to find everything starting from 'inc/api...
+// const PREG_APIURL = new RegExp(/["|'\/](inc\/api.*?)["|']/); 
 
-var apiUrlCallback = function(full, part) {
-  return full.replace(part, build.config.path.demo_api_url + part);
+// Find anything between ' and inc/api
+const PREG_APIURL = new RegExp(/["|'\/](http:\/\/.*?)inc\/api/);
+
+var apiUrlCallback = function (full, part) {
+  // Prepend host url
+  // return full.replace(part, build.config.path.demo_api_url + part); 
+
+  // Replace host url
+  return full.replace(part, build.config.path.demo_api_url);
+
 };
 
 // Gulp task to find api path and convert to absolute url
-gulp.task('apiurl', function() {
-  build.config.dist.forEach(function(path) {
+gulp.task('apiurl', function () {
+  build.config.dist.forEach(function (path) {
     var output = '';
     if (path.indexOf('**') !== -1) {
-      func.getDemos().forEach(function(demo) {
+      func.getDemos().forEach(function (demo) {
         output = path;
         output = output.replace('**', demo);
         gulp.src(output + '/**/*js').pipe(replace(PREG_APIURL, apiUrlCallback)).pipe(gulp.dest(output));
